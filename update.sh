bcf9c3d7b4328b1b489f763261eff6e248532aab
Petri Kallberg <210689955+petrikallberg-firemind@users.noreply.github.com>
2025-09-08 21:25:32 +0300

    Ignore error if stack exists

--- update.sh
+++ update.sh
@@ -6,9 +6,14 @@ TEMPLATE=template.yaml
 aws cloudformation create-stack \
   --stack-name ${STACK_NAME} \
   --template-body file://${TEMPLATE} \
-  --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM
+  --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM || true
 
 aws cloudformation wait stack-create-complete \
   --stack-name ${STACK_NAME}
 
+FUNCTION_URL=$(aws cloudformation describe-stacks \
+  --stack-name ${STACK_NAME} \
+  --query 'Stacks[0].Outputs[]' | jq -r '.[] | select(.OutputKey=="FunctionUrl").OutputValue')
+
+echo ${FUNCTION_URL}
 
