09c58e99ce4d2d2f4e9c5e6b15c9a0dd70c9b7dd
Petri Kallberg <petrikallberg@Petris-Mac-mini.localdomain>
2025-09-14 18:55:53 +0300

    Wait logic is built in to cloudformation deploy

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ TEMPLATE=template.yaml
 
 for R in $(cat runtimes)
 do
+  echo "Lambda runtime ${R} ..."
   aws cloudformation deploy \
     --stack-name ${STACK_NAME} \
     --template-file ${TEMPLATE} \
@@ -13,27 +14,11 @@ do
     --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --no-fail-on-empty-changeset
 
-  aws cloudformation wait stack-exists\
-    --stack-name ${STACK_NAME}
-
-  # Wait until stack is created or updated
-
-  while true
-  do
-    STACK_STATUS=$(aws cloudformation describe-stacks \
-      --stack-name ${STACK_NAME} \
-      --query 'Stacks[0].StackStatus' \
-      --output text)
-    echo "Stack status $(STACK_STATUS)"
-    [[ "$(STACK_STATUS)" == *_COMPLETE ]] && break
-    sleep 5
-  done
-
   FUNCTION_URL=$(aws cloudformation describe-stacks \
     --stack-name ${STACK_NAME} \
     --query 'Stacks[0].Outputs[]' | jq -r '.[] | select(.OutputKey=="FunctionUrl").OutputValue')
 
-  curl ${FUNCTION_URL} -o ${R}-requirements.txt
+  curl --no-progress-meter -o ${R}-requirements.txt ${FUNCTION_URL}
 done
 
 aws cloudformation delete-stack \
