147206fb0c91d0a8115fc1d1d94a56bd28de1bf6
Petri Kallberg <petrikallberg@Petris-Mac-mini.localdomain>
2025-09-14 17:40:37 +0300

    use cloudformation deploy for stack creation and update

--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@ RUNTIMES=./runtimes
 STACK_NAME=python-runtime-dump
 TEMPLATE=template.yaml
 
-aws cloudformation create-stack \
-  --stack-name ${STACK_NAME} \
-  --template-body file://${TEMPLATE} \
-  --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM || true
+#aws cloudformation create-stack \
+#  --stack-name ${STACK_NAME} \
+#  --template-body file://${TEMPLATE} \
+#  --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM || true
 
-aws cloudformation wait stack-create-complete \
-  --stack-name ${STACK_NAME}
+#aws cloudformation wait stack-create-complete \
+#  --stack-name ${STACK_NAME}
 
 FUNCTION_URL=$(aws cloudformation describe-stacks \
   --stack-name ${STACK_NAME} \
@@ -18,14 +18,21 @@ FUNCTION_URL=$(aws cloudformation describe-stacks \
 
 for R in $(cat runtimes)
 do
-  aws cloudformation update-stack \
+  aws cloudformation deploy \
     --stack-name ${STACK_NAME} \
-    --use-previous-template \
+    --template-body file://${TEMPLATE} \
     --parameters ParameterKey=PythonRuntime,ParameterValue=${R} \
-    --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --no-fail-on-empty-changeset
+
+  sleep 5
 
   aws cloudformation wait stack-update-complete \
     --stack-name ${STACK_NAME}
+
+  FUNCTION_URL=$(aws cloudformation describe-stacks \
+    --stack-name ${STACK_NAME} \
+    --query 'Stacks[0].Outputs[]' | jq -r '.[] | select(.OutputKey=="FunctionUrl").OutputValue')
 
   curl ${FUNCTION_URL} -o ${R}-requirements.txt
 done
