dac119578d262b77c884c1a3430d54a3db0c01e1
Petri Kallberg <petrikallberg@Petris-Mac-mini.localdomain>
2025-09-14 17:04:30 +0300

    Loop runtimes

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+RUNTIMES=./runtimes
 STACK_NAME=python-runtime-dump
 TEMPLATE=template.yaml
 
@@ -15,5 +16,19 @@ FUNCTION_URL=$(aws cloudformation describe-stacks \
   --stack-name ${STACK_NAME} \
   --query 'Stacks[0].Outputs[]' | jq -r '.[] | select(.OutputKey=="FunctionUrl").OutputValue')
 
-echo ${FUNCTION_URL}
+for R in $(cat runtimes)
+do
+  aws cloudformation update-stack \
+    --stack-name ${STACK_NAME} \
+    --use-previous-template \
+    --parameters \
+    --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM
 
+  aws cloudformation wait stack-update-complete \
+    --stack-name ${STACK_NAME}
+
+  curl ${FUNCION_URL} -o ${R}-requirements.txt
+done
+
+aws cloudformation delete-stack \
+  --stack-name ${STACK_NAME}
